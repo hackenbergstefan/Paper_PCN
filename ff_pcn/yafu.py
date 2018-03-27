d438ac041662905640caf49a6ef0a8013f690c19
Stefan Hackenberg <mail@stefan-hackenberg.de>
2018-03-27 11:35:00 +0200

    Improve

--- ff_pcn/yafu.py
+++ ff_pcn/yafu.py
@@ -1,25 +1,54 @@
-#! /usr/bin/env python
+#!/usr/bin/env python2
+
+try:
+    import ff_pcn
+except ImportError:
+    import sys
+    import os
+    sys.path.append(os.path.abspath(os.path.join(__file__, '../../')))
 
 import sys
 import os
+import shutil
 import subprocess
 import logging
 import multiprocessing
 
 
 YAFU_WORK_FOLDER = './yafu_job'
+"""Path to working folder."""
 
-TIMEOUT = 10*60
+YAFU_EXECUTABLE = './yafu-setup-package/prefix/bin/yafu'
+"""Path to yafu setup package"""
+
+TIMEOUT = 10
 
 
-def factor_with_yafu(num, timeout=TIMEOUT):
-    with open('job.bat', 'w') as fp:
-        fp.write('factor(%d)\n' % num)
+def factor_with_yafu(num,
+                     timeout=TIMEOUT,
+                     yafu_executable=YAFU_EXECUTABLE,
+                     yafu_work_folder=YAFU_WORK_FOLDER,
+                     append_to=None):
 
+    if os.path.exists(yafu_work_folder):
+        shutil.rmtree(yafu_work_folder)
+    os.mkdir(yafu_work_folder)
+
+    logging.critical('Start: %d', num)
+
+    cmd = [
+        os.path.abspath(yafu_executable),
+        'factor(%d)' % num,
+        '-of',
+        'out.txt',
+        '-threads',
+        '%d' % multiprocessing.cpu_count(),
+        '-silent'
+    ]
+    logging.getLogger(__name__).debug('Popen %s', ' '.join(cmd))
     proc = subprocess.Popen(
-        ['yafu', '-batchfile', 'job.bat', '-of', 'out.txt', '-threads', '%d' % multiprocessing.cpu_count()],
-        cwd=YAFU_WORK_FOLDER,
-        stdout=subprocess.PIPE,
+        cmd,
+        cwd=yafu_work_folder,
     )
 
     try:
@@ -27,9 +56,21 @@ def factor_with_yafu(num, timeout=TIMEOUT):
     except subprocess.TimeoutExpired:
         proc.kill()
         proc.communicate()
+        logging.critical('Abort')
     else:
-        logging.info('Finished: %s', open(YAFU_WORK_FOLDER+'/out.txt').readlines())
+        out = open(yafu_work_folder+'/out.txt').read()
+        logging.critical('Finished: %s', out)
+        if append_to:
+            with open(append_to, 'a') as fp:
+                fp.write(out+'\n')
+
+
+def factor_batch_with_yafu(batch):
+    nums = [int(n) for n in open(batch).readlines()]
+    for num in nums:
+        factor_with_yafu(num, append_to=batch+'.out')
 
 
 if __name__ == '__main__':
-    factor_with_yafu(int(sys.argv[1]))
+    logging.basicConfig(level=logging.INFO)
+    factor_batch_with_yafu(sys.argv[1])
