075d0dd1addb29cc9aa90d7047aabae0bfb88ab9
Stefan Hackenberg <mail@stefan-hackenberg.de>
2018-03-27 09:05:36 +0200

    Add yafu.py

---
+++ ff_pcn/yafu.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import subprocess
+import logging
+
+
+YAFU_WORK_FOLDER = './yafu_job'
+
+TIMEOUT = 10*60
+
+
+def factor_with_yafu(num, timeout=TIMEOUT):
+    with open('job.bat', 'w') as fp:
+        fp.write('factor(%d)\n' % num)
+
+    proc = subprocess.Popen(
+        ['yafu', '-batchfile', 'job.bat', '-of', 'out.txt', '-threads', '%d' % os.cpu_count()],
+        cwd=YAFU_WORK_FOLDER,
+        stdout=subprocess.PIPE,
+    )
+
+    try:
+        proc.communicate(timeout=timeout)
+    except subprocess.TimeoutExpired:
+        proc.kill()
+        proc.communicate()
+    else:
+        logging.info('Finished: %s', open(YAFU_WORK_FOLDER+'/out.txt').readlines())
+
+
+if __name__ == '__main__':
+    factor_with_yafu(sys.argv[1])
