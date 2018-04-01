162822ea28da36004bc4acb9c4fb899220db9564
Stefan Hackenberg <mail@stefan-hackenberg.de>
2018-04-01 08:24:49 +0200

    Switch to cyclotomic numbers database.

--- ff_pcn/factorer.py
+++ ff_pcn/factorer.py
@@ -6,15 +6,23 @@
 __author__ = "Stefan Hackenberg"
 
 
+try:
+    import ff_pcn
+except ImportError:
+    import sys
+    import os
+    sys.path.append(os.path.abspath(os.path.join(__file__, '../../')))
 import logging
 import os
 import sys
 import re
 import csv
-from sage.all import factor, Integer, prod
+from sage.all import factor, Integer, prod, cyclotomic_polynomial
+from ff_pcn.cyclotomic_numbers_database import get_factorization as get_factorization_from_online_database
+from ff_pcn.basic_number_theory import cyclotomic_equivalents
 
 
-FACTOR_DATABASE = os.path.abspath(os.path.join(__file__, '../factors.csv'))
+FACTOR_DATABASE = os.path.abspath(os.path.join(__file__, '../cyclotomic_numbers.csv'))
 
 
 def facprod(fac):
@@ -28,16 +36,34 @@ class Factorer(object):
         self.load()
         self.queue = []
 
-    def add(self, num, factorization=None):
-        self.database[num] = factorization
+    def add(self, nb, num, factorization):
+        self.database[nb] = factorization
         self.save()
 
-    def get(self, num):
+    def get(self, nb):
+        num = cyclotomic_polynomial(nb[0])(nb[1])
         if num < 1e10:
             return list(factor(num))
-        if num in self.database:
-            return self.database[num]
-        logging.getLogger(__name__).critical('Factorer.get: Factorization needed : %d', num)
+
+        equivalents = cyclotomic_equivalents(*nb)
+
+        # Lookup local database
+        for mb in equivalents:
+            if mb in self.database:
+                if nb != mb:
+                    self.database[nb] = self.database[mb]
+                    del self.database[mb]
+                return self.database[nb]
+
+        # Lookup online database
+        for mb in equivalents:
+            fac = get_factorization_from_online_database(*mb)
+            logging.getLogger(__name__).critical('Factorer.get: online lookup: %s %s', mb, fac)
+            if fac is not None:
+                self.add(nb, num, fac)
+                return fac
+
+        logging.getLogger(__name__).critical('Factorer.get: Factorization needed : %s %d', nb, num)
         return None
 
     def save(self):
@@ -48,14 +74,15 @@ class Factorer(object):
     def load(self):
         with open(FACTOR_DATABASE, 'r') as fp:
             reader = csv.reader(fp)
-            for num, fac in reader:
+            for nb, fac in reader:
                 try:
-                    num = Integer(num)
+                    nb = eval(nb)
                     fac = eval(fac)
+                    num = cyclotomic_polynomial(nb[0])(nb[1])
                     assert num == facprod(fac)
-                    self.database[num] = fac
+                    self.database[nb] = fac
                 except SyntaxError:
-                    logging.critical('Unable to eval: %s %s', num, fac)
+                    logging.critical('Unable to eval: %s %s', nb, fac)
                     raise
         logging.getLogger(__name__).debug('Factorer.load: Loaded %s', self.database)
 
@@ -64,17 +91,19 @@ class Factorer(object):
         Read yafu output file. Line format: (NUMBER)/FAC1/FAC2/...
         """
         for line in open(yafu_out_fil).readlines():
-            num = re.search(r'^\((\d+)\)', line)
-            if not num:
+            match = re.search(r'^\((\d+), (\d+)\) \((\d+)', line)
+            if not match:
                 continue
-            num = int(num.group(1))
+            n = int(match.group(1))
+            b = int(match.group(2))
+            num = int(match.group(3))
             facs = [(Integer(r), Integer(m or 1)) for r, m in re.findall(r'/(\d+)\^?(\d+)?', line)]
 
             num2 = facprod(facs)
             assert num == num2, '%d != %d = prod(%s)' % (num, num2, facs)
 
             facs = cleanup_factorization(facs)
-            self.add(num, facs)
+            self.add((n, b), num, facs)
         self.save()
 
 
